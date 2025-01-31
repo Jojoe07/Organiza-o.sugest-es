cc419ea95916f59204e0fa5428c0fbde4d862003
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-01-30 23:36:26 -0300

    Add files via upload

---
+++ Página_inicial.py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Organização das sugestões " ,
+    page_icon="🏆" ,
+)
+
+st.title("Sugestões/ jornada")
+st.sidebar.success("Escolha uma página")
+
+("""
+
+
+ **Objetivo**
+ 
+ Sabendo que nossa conta é voltada apenas para a utilização mobile,
+ surgiram algumas ideias que podem ser utilizadas para aumentar a rentabilidade e auxilio para contorno de objeções.
+ """)
+
+st.caption("Desenvolvido por Joenice Almeida")
+st.caption("Em 13/01/2025")
