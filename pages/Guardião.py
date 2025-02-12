f3a6ac94b99e296866334c24a6c8e57681a6f2d2
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-02-12 07:08:49 -0300

    Update Guardião.py

--- pages/Guardião.py
+++ pages/Guardião.py
@@ -39,7 +39,7 @@ tab1.write("""
  """) #markdown
 
 
-tab2.video("/workspaces/Organiza-o.sugest-es/pages/Guardião_att.mp4")
+tab2.video("Guardião_att.mp4")
 
 embed_code = """ <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="500" height="450" src="https://embed.figma.com/proto/GdWmLrOSmBw64bXGVbczPD/Faq-Emps?node-id=142-25&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=142%3A25&show-proto-sidebar=1&embed-host=share" allowfullscreen></iframe>"""
 st.components.v1.html(embed_code,height=400)
